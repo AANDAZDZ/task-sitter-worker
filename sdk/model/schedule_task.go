4a6252ffb9d7d28d315cf8de7705ee5719b60a0d
zhangdazhuang <zhangdazhuang@360.cn>
2024-01-05 11:19:32 +0800

    feat: add fixed rate task type

--- sdk/model/schedule_task.go
+++ sdk/model/schedule_task.go
@@ -4,13 +4,16 @@ type ScheduleTaskReq struct {
 	TaskName      string
 	TaskType      string
 	TaskContext   string
+	ScheduleType  int32
 	BeginTime     int64
 	EndTime       int64
 	RetryNum      int32
 	RetryInterval int64
 	BizName       string
+	RateInterval  int64
 }
 
 type ScheduleTaskResp struct {
+	ErrNo   int
 	Message string
 }
